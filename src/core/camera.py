87fb5d2a1e4aefb9fbc8c26f98a6411de19c633a
Rusty <golovinartem2004@gmail.com>
2022-11-23 10:48:59 +0400

    Camera fields marked as private

--- src/core/camera.py
+++ src/core/camera.py
@@ -9,52 +9,60 @@ from src.core.types import PlayerInfo
 
 class Camera:
     def __init__(self):
-        self.offset_x = config.screen.size[0] / 2
-        self.offset_y = config.screen.size[1] / 2
-        self.speed = 1
-        self.initial_position_set = False
+        self._offset_x = config.screen.size[0] / 2
+        self._offset_y = config.screen.size[1] / 2
+        self._speed = 1
+        self._initial_position_set = False
+
+    @property
+    def offset_x(self):
+        return self._offset_x
+
+    @property
+    def offset_y(self):
+        return self._offset_y
 
     def move(self, x, y):
-        self.offset_x += x * self.speed
-        self.offset_y += y * self.speed
+        self._offset_x += x * self._speed
+        self._offset_y += y * self._speed
         self.validate_position()
 
     def increase_speed(self):
-        self.speed += config.camera.step_faster
-        self.speed = min(config.camera.max_speed, self.speed)
+        self._speed += config.camera.step_faster
+        self._speed = min(config.camera.max_speed, self._speed)
 
     def decrease_speed(self):
-        self.speed -= config.camera.step_slower
-        self.speed = max(config.camera.min_speed, self.speed)
+        self._speed -= config.camera.step_slower
+        self._speed = max(config.camera.min_speed, self._speed)
 
     def validate_position(self):
         world_size = config.world.size
-        if self.offset_x < -world_size + config.screen.size[0]:
-            self.offset_x = -world_size + config.screen.size[0]
-        if self.offset_x > world_size:
-            self.offset_x = world_size
-        if self.offset_y < -world_size + config.screen.size[1]:
-            self.offset_y = -world_size + config.screen.size[1]
-        if self.offset_y > world_size:
-            self.offset_y = world_size
+        if self._offset_x < -world_size + config.screen.size[0]:
+            self._offset_x = -world_size + config.screen.size[0]
+        if self._offset_x > world_size:
+            self._offset_x = world_size
+        if self._offset_y < -world_size + config.screen.size[1]:
+            self._offset_y = -world_size + config.screen.size[1]
+        if self._offset_y > world_size:
+            self._offset_y = world_size
 
     def set_center(self, center: tuple[float, float]):
-        self.offset_x, self.offset_y = center[0] + config.screen.size[0] / 2, center[1] + config.screen.size[1] / 2
+        self._offset_x, self._offset_y = center[0] + config.screen.size[0] / 2, center[1] + config.screen.size[1] / 2
         self.validate_position()
 
     def get_in_game_mouse_position(self) -> tuple[float, float]:
         mouse_pos = pygame.mouse.get_pos()
-        return mouse_pos[0] - self.offset_x, mouse_pos[1] - self.offset_y
+        return mouse_pos[0] - self._offset_x, mouse_pos[1] - self._offset_y
 
     def to_screen_position(self, pos: tuple[float, float]):
-        return pos[0] + self.offset_x, pos[1] + self.offset_y
+        return pos[0] + self._offset_x, pos[1] + self._offset_y
 
     def distance_to(self, position: tuple[float, float]):
-        return math.sqrt((position[0] + self.offset_x - config.screen.width / 2) ** 2 + (
-                position[1] + self.offset_y - config.screen.height / 2) ** 2)
+        return math.sqrt((position[0] + self._offset_x - config.screen.width / 2) ** 2 + (
+                position[1] + self._offset_y - config.screen.height / 2) ** 2)
 
     def check_if_fortress_appeared(self, ecs: EntityComponentSystem, player_owner: PlayerInfo):
-        if self.initial_position_set:
+        if self._initial_position_set:
             return
         from src.components.base.player_owner import PlayerOwnerComponent
         from src.components.base.position import PositionComponent
@@ -65,4 +73,4 @@ class Camera:
                                                                     PlayerOwnerComponent)):
             if owner.socket_id == player_owner.socket_id:
                 self.set_center((-position.x, -position.y))
-                self.initial_position_set = True
+                self._initial_position_set = True
